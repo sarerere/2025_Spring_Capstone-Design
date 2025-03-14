f62cdbc0874e61e240279f0be1060d3988dd42d1
ancestor9(조상구) <46776608+ancestor9@users.noreply.github.com>
2025-03-14 21:32:46 +0900

    Add files via upload

--- Weeko3/jinja/main.py
+++ Weeko3/jinja/main.py
@@ -13,9 +13,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # 템플릿 경로 설정
 templates = Jinja2Templates(directory="templates")
 
+# @app.get("/", response_class=HTMLResponse)
+# async def read_root(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
+async def read_root(request: Request): 
+    # Request 객체는 클라이언트의 요청 정보
+    # - 클라이언트가 FastAPI 서버에 요청을 보낼 때 자동 생성
+    # "index.html"를 랜더링하고 templates에 {"request": request}을 전달
     return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.get("/bs4", response_class=HTMLResponse)
 async def read_bs4_doc(request: Request):
